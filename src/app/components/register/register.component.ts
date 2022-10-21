9b7f0b0e5cc3bd182406a5a4d505730919f5de0d
Enowizzy <enockyona906@gmail.com>
2022-10-21 08:01:20 +0300

    register commit

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  
+  get first_name() {
+    return this.registrationForm.get('first_name');
   }
+  get last_name() {
+    return this.registrationForm.get('last_name');
+  }
+  get email() {
+    return this.registrationForm.get('email');
+  }
+  get password() {
+    return this.registrationForm.get('password');
+  }
+  constructor(private fb: FormBuilder) {}
 
+  ngOnInit(): void {}
+
+  registrationForm = this.fb.group({
+    first_name: ['', [Validators.required, Validators.minLength(3)]],
+    last_name: ['', [Validators.required, Validators.minLength(3)]],
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(8)]],
+  });
+
+  onSubmit() {
+    console.log(this.registrationForm.value);
+  }
 }
