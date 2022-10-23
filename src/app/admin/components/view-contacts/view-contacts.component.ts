9b6191e5578776bae70098a408552e40630dab08
Jeedie JIDE <enockyona906@gmail.com>
2022-10-23 12:56:53 +0300

    pull data from API

--- src/app/admin/components/view-contacts/view-contacts.component.ts
+++ src/app/admin/components/view-contacts/view-contacts.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Contact } from 'src/app/models/contact.model';
+import { ContactService } from 'src/app/services/contact.service';
 
 @Component({
   selector: 'app-view-contacts',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewContactsComponent implements OnInit {
 
-  constructor() { }
+  contactData1: any;
+  contactDatas: any;
+  user = new Contact();
+  target: string = '';
+  constructor(private contactData: ContactService
+    ) {}
 
   ngOnInit(): void {
+   
+    this.contactData1 = this.contactData.getContactData();
+    this.getContact();
+  }
+
+  getContact() {
+    this.contactData.getContactData().subscribe((res) => {
+      this.contactDatas = res;
+    });
+  }
+  storeContact() {
+    this.contactData.storeContactData(this.user).subscribe((res: any) => {
+      this.contactDatas = res;
+      if (res.code == 1) {
+        this.target =
+          '<div class="alert alert-success">Success!' + res.message + '</div>';
+      } else if (res.code == 2) {
+        this.target =
+          '<div class="alert alert-danger">Error!' + res.message + '</div>';
+      }
+      this.getContact();
+    });
   }
 
 }
