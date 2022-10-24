3d092c9e6109a4e9c3c913fcc8d86aecf0dbd76b
Enowizzy <enockyona906@gmail.com>
2022-10-24 11:37:13 +0300

    sucess and error message commit

--- src/app/admin/components/view-contacts/view-contacts.component.ts
+++ src/app/admin/components/view-contacts/view-contacts.component.ts
@@ -1,23 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { NgToastService } from 'ng-angular-popup';
 import { Contact } from 'src/app/models/contact.model';
 import { ContactService } from 'src/app/services/contact.service';
 
 @Component({
   selector: 'app-view-contacts',
   templateUrl: './view-contacts.component.html',
-  styleUrls: ['./view-contacts.component.css']
+  styleUrls: ['./view-contacts.component.css'],
 })
 export class ViewContactsComponent implements OnInit {
-
   contactData1: any;
   contactDatas: any;
   user = new Contact();
   target: string = '';
-  constructor(private contactData: ContactService
-    ) {}
+  constructor(
+    private contactData: ContactService,
+    private toast: NgToastService
+  ) {}
 
   ngOnInit(): void {
-   
     this.contactData1 = this.contactData.getContactData();
     this.getContact();
   }
@@ -41,17 +42,26 @@ export class ViewContactsComponent implements OnInit {
     });
   }
 
-  deleteContact(id: number){
+  deleteContact(id: number) {
     return this.contactData.deleteContact(id).subscribe((res: any) => {
       this.getContact();
       if (res.code == 1) {
+        this.toast.success({
+          detail: "Success Message",
+          summary: res.message,
+          duration: 5000,
+        });
         this.target =
           '<div class="alert alert-success">Success!' + res.message + '</div>';
       } else if (res.code == 2) {
+        this.toast.error({
+          detail: "Error Message",
+          summary: res.message,
+          duration: 5000,
+        });
         this.target =
           '<div class="alert alert-danger">Error!' + res.message + '</div>';
       }
     });
   }
-
 }
