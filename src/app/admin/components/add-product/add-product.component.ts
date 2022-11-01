5230a0ef3571d2b5215c7b5b229aafbd206b7d8f
Enowizzy <enockyona906@gmail.com>
2022-11-01 09:44:42 +0300

    finishing add product component

--- src/app/admin/components/add-product/add-product.component.ts
+++ src/app/admin/components/add-product/add-product.component.ts
@@ -63,7 +63,7 @@ export class AddProductComponent implements OnInit {
       if (res.code == 1) {
         this.target =
           '<div class="alert alert-success">Success!' + res.message + '</div>';
-          this.route.navigate(['/']);
+          this.route.navigate(['/product-list']);
 
       } else if (res.code == 2) {
         this.target =
