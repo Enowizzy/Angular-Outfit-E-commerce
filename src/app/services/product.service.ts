c86a805f3d1e666c9254a0e9b9b69b2dc4bf78ac
Enowizzy <enockyona906@gmail.com>
2022-11-01 09:26:53 +0300

    finish product service

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  private API_URL = environment.API_URL;
+  constructor(private _http: HttpClient) { }
 
-  constructor() { }
+  addProduct(data: any): Observable<any> {
+    return this._http.post(this.API_URL + 'addProduct', data);
+  }
 }
