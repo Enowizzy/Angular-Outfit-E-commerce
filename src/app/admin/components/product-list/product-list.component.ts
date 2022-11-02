3ce4036de3f036c2f31831c9ef628704738e164e
Jeedie JIDE <enockyona906@gmail.com>
2022-11-02 19:40:21 +0300

    add admin components

--- src/app/admin/components/product-list/product-list.component.ts
+++ src/app/admin/components/product-list/product-list.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import { NgToastService } from 'ng-angular-popup';
 
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.css']
+  styleUrls: ['./product-list.component.css'],
 })
 export class ProductListComponent implements OnInit {
+  constructor(private toast: NgToastService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
