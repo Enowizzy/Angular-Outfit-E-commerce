6367c04d75580b4d2f9b879f81c08c6aeaa1a65f
Jeedie JIDE <enockyona906@gmail.com>
2022-12-04 09:14:25 +0300

    catch error

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,6 +1,6 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 @Injectable({
@@ -11,6 +11,9 @@ export class ProductService {
   constructor(private _http: HttpClient) { }
 
   addProduct(data: any): Observable<any> {
-    return this._http.post(this.API_URL + 'addProduct', data);
+    return this._http.post(this.API_URL + 'addProduct', data).pipe(catchError(this.handleError));
+  }
+  handleError(error: HttpErrorResponse) {
+    return throwError(error);
   }
 }
